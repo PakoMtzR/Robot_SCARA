f2f8c6013d13f36f637351c032d536e41cb57ec4
PakoMtzR <francisco.2k1@gmail.com>
2025-01-02 14:31:07 -0600

    Modularizamos el codigo de los motores con encoder

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 # Robot SCARA
+![Robot Scara](https://www.researchgate.net/profile/Felipe-Torres/publication/335149942/figure/fig1/AS:791560924954624@1565734239026/Figura-1-Robot-manipulador-SCARA.jpg)
+
+## 📖 Descripción
+Este proyecto implementa el control, el cálculo de trayectorias y otras funcionalidades clave para un robot SCARA. Incluye:
+- Modelado cinemático y dinámico del robot.
+- Control de juntas basado en PID.
+- Generación de trayectorias.
+- Integración con hardware (motores, encoders y sensores).
+- Herramientas de simulación y visualización.
 
 ## Material de apoyo
-
-- Pinout RaspberryPi-Pico
-  
-![raspberrypi-pico-pinout](/imgs/raspberry-pi-pico-pinout.png)
-
-- Pinout tb6612fng
-
-![raspberrypi-pico-pinout](/imgs/tb6612fng-esquema.webp)
-
-- Pinout Encoder motor
-
-![raspberrypi-pico-pinout](/imgs/AR3100-GM-25-370-Encoder.jpg)
+<img src="imgs/raspberry-pi-pico-pinout.png" height="100">
+<img src="imgs/tb6612fng-esquema.webp" height="100">
+<img src="imgs/AR3100-GM-25-370-Encoder.jpg" height="100">
 
 ### Links
-[Raspberry Pi Pico - MicroPython Environment](https://datasheets.raspberrypi.com/pico/raspberry-pi-pico-python-sdk.pdf)
+- [Raspberry Pi Pico - MicroPython Environment](https://datasheets.raspberrypi.com/pico/raspberry-pi-pico-python-sdk.pdf)
+- [MicroPython Docs](https://docs.micropython.org/en/latest/rp2/quickref.html)
 
-[MicroPython Docs](https://docs.micropython.org/en/latest/rp2/quickref.html)
-
-### Tutorials
-[Control PID Tutorial (motor encoder position)](https://www.youtube.com/watch?v=dTGITLnYAY0)
+### Tutoriales
+- [Control PID Tutorial (motor encoder position)](https://www.youtube.com/watch?v=dTGITLnYAY0)
--- motor_encoder.py
+++ motor_encoder.py
@@ -1,53 +1,82 @@
 from machine import Pin, PWM
 import time
 
-# Definimos los pines del encoder
-encoder_pin_a = Pin(18, Pin.IN, Pin.PULL_DOWN)  # 18
-encoder_pin_b = Pin(16, Pin.IN, Pin.PULL_DOWN)  # 16
+class MotorEncoder:
+    def __init__(self, encoder_pin_a:int, encoder_pin_b:int, pin_pwm:int, motor_input_a:int, motor_input_b:int) -> None:
+        # Definimos los pines del encoder
+        self.encoder_pin_a = Pin(encoder_pin_a, Pin.IN, Pin.PULL_DOWN)  # 18
+        self.encoder_pin_b = Pin(encoder_pin_b, Pin.IN, Pin.PULL_DOWN)  # 16
 
-# Definimos los pines para controlar direccion del motor
-motor_input_a = Pin(10, Pin.OUT)
-motor_input_b = Pin(11, Pin.OUT)
+        # Definimos los pines para controlar direccion del motor
+        self.motor_input_a = Pin(motor_input_a, Pin.OUT)    # 10
+        self.motor_input_b = Pin(motor_input_b, Pin.OUT)    # 11
 
-# Definimos pin para en control del PWM
-pin_pwm = PWM(Pin(12), freq=2000, duty_u16=0)
+        # Definimos pin para en control del PWM
+        self.pin_pwm = PWM(Pin(pin_pwm), freq=2000, duty_u16=0)  # 12
 
-# Variables del encoder 
-encoder_counter = 0
-target_pulses = 11*45
+        # Variables del encoder y PID
+        self.encoder_counter = 0
+        self.kp = 5
+        self.kd = 0.1
+        self.ki = 0
+        self.previous_error = 0
+        self.integral = 0
+        self.previous_time = time.ticks_ms()
 
-# Funcion para la lectura del encoder
-def read_encoder(pin):
-    global encoder_counter, running
-    encoder_counter += 1 if encoder_pin_a.value() == encoder_pin_b.value() else -1
-    print("encoder_counter:", encoder_counter)
-    if encoder_counter >= target_pulses:
-        set_motor()
-        running = False
+        # Configuración de interrupción para el encoder
+        self.encoder_pin_a.irq(trigger=Pin.IRQ_RISING, handler=self.read_encoder)
 
-# Funcion para configurar giro del motor
-def set_motor(direction=0, pwm_percent_value=0):
-    # Verificamos valor de porcentaje de pwm [0% - 100%]
-    pwm_percent_value = max(0, min(100, pwm_percent_value))
+    def read_encoder(self, pin) -> None:
+        """
+        Funcion para la lectura del encoder
+        """
+        self.encoder_counter += 1 if self.encoder_pin_a.value() == self.encoder_pin_b.value() else -1
+        print("encoder_counter:", self.encoder_counter)
 
-    # Configurar valor del pwm
-    pin_pwm.duty_u16((pwm_percent_value*65_535)//100)     
+    def set_motor(self, direction:int=0, pwm_percent_value:int=0) -> None:
+        """
+        Funcion para configurar el sentido de giro del motor \n
+        direction = [-1,1] \n
+        pwm_percent_value = [0-100]%
+        """
+        # Verificamos valor de porcentaje de pwm [0% - 100%]
+        pwm_percent_value = max(0, min(100, pwm_percent_value))
 
-    if direction == 1:
-        motor_input_a.on()
-        motor_input_b.off()
-    elif direction == -1:
-        motor_input_b.on()
-        motor_input_a.off()
-    else:
-        motor_input_a.off()
-        motor_input_b.off()
+        # Configurar valor del pwm
+        self.pin_pwm.duty_u16((pwm_percent_value*65_535)//100)     
 
-# Configuración de interrupción para el encoder
-encoder_pin_a.irq(trigger=Pin.IRQ_RISING, handler=read_encoder)
+        if direction == 1:
+            self.motor_input_a.on()
+            self.motor_input_b.off()
+        elif direction == -1:
+            self.motor_input_b.on()
+            self.motor_input_a.off()
+        else:
+            self.motor_input_a.off()
+            self.motor_input_b.off()
 
-# Bucle principal
-running = True
-set_motor(1,30)
-while running:
-    time.sleep_ms(10)  # Pausa de 100 ms para darle tiempo al sistema
+    # Controlador PID
+    def pid_control(self, target_degrees:int) -> None:        
+        # Calcular el intervalo de tiempo (delta t)
+        current_time = time.ticks_ms()
+        delta_time = time.ticks_diff(current_time, self.previous_time)/1000 # Convertimos a segundos
+        previous_time = current_time
+
+        # Calculamos la cantidad de pulsos necesarios para llegar al angulo deseado
+        target_pulses = (target_degrees*495)//360
+
+        # Calculo del error
+        error = target_pulses - self.encoder_counter
+        self.integral += error*delta_time
+        derivative = (error-self.previous_error) / delta_time if delta_time > 0 else 0
+        self.previous_error = error
+
+        # Calcular la señal de control
+        control_signal = self.kp*error + self.ki*self.integral + self.kd*derivative
+        # print(control_signal)
+        # pwm_value = abs(int(control_signal))
+        pwm_value = max(0, min(100, abs(int(control_signal))))
+        direction = 1 if control_signal > 0 else -1
+
+        # Ajustar el motor en función de la señal de control
+        self.set_motor(direction, pwm_value)
