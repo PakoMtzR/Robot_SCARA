cd5041160f39759d5e978e2a484a932490f747a4
PakoMtzR <francisco.2k1@gmail.com>
2025-01-14 11:41:18 -0600

    Intentamos arreglarlo

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from machine import UART, Pin
 import time
 from motor_encoder import MotorEncoder
 from interrupts import LimitSwitch
+
+from machine import UART
+
+# Configuración del puerto serial
+uart = UART(0, baudrate=9600, tx=0, rx=1)
 
 # Declaramos los motores con encoder
 motor_1 = MotorEncoder(21, 20, 13, 14, 15)
@@ -14,20 +18,24 @@ motor_3.kp = 2
 motor_3.kd = 0.2
 
 # Declaramos angulos objetivos
-target_degrees_1 = 100
+target_degrees_1 = -1000
 target_degrees_2 = -100
 target_degrees_3 = 100
 
 while True:
-    data = input().strip().split(",")
-    if data[0] == "D" and len(data) == 4:
-        angle_values = [int(x) for x in data[1::]]
+    if uart.any():  # Verifica si hay datos en el buffer de entrada
+        data = uart.read()  # Lee los datos disponibles
+        print("Datos recibidos:", data) 
+    # data = input().strip().split(",")
+    # print(data)
+    # if data[0] == "D" and len(data) == 4:
+    #     angle_values = [int(x) for x in data[1::]]
     # else:
     #     angle_values = [0 for _ in range(3)]
     # print(angle_values)
 
     # Ejecuta el PID en cada iteración
-    motor_1.pid_control(target_degrees_1) 
+    # motor_1.pid_control(target_degrees_1) 
     # motor_1.pid_control(angle_values[0])   
     # motor_2.pid_control(angle_values[1])
     # motor_3.pid_control(angle_values[2])
